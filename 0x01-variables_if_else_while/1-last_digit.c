6c729a21f42e34144607ec8a2ecb68ca1c6e78d6
Temitopemoses <101447242+Temitopemoses@users.noreply.github.com>
2022-04-10 10:35:01 -0700

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,13 +11,23 @@
  */
 int main(void)
 {
-int n;
+int n, lastd;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("last digit of %d is %d %s\n", n, n % 10,
-((n % 10) == 0) ? "and is 0"
-: (((n % 10) > 5) ? "and is greater than 5"
-: "and is less than 6 and not 0"));
+lastd = n % 10;
+ 
+ if (lastd > 5)
+ {
+   printf("last digit of %d is %d and is greater than 5\n", n, lastd);
+ }
+ else if (lastd == 0)
+ {
+   printf("last digit of %d is %d and is 0\n", n, lastd);
+ {
+ else if (lastd  < 6 && lastd != 0)
+ {
+   printf(last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
+ }
 return (0);
 }
