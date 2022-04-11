135c869e4f0f5a423d9400d34f5b7cdab1b54872
Temitopemoses <101447242+Temitopemoses@users.noreply.github.com>
2022-04-10 18:08:02 -0700

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,7 +2,7 @@
 /**
  * main - entry point
  * 
- * Description: Print lower and lower alphabet on the same line
+ * Description: Print lower and upper alphabet on the same line
  * Return: 0
  */
 int main(void)
