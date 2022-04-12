ca96c1d270f260cae5239f3705e4e24de5d5a31f
Temitopemoses <101447242+Temitopemoses@users.noreply.github.com>
2022-04-12 09:15:28 -0700

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints all possible combinations of two different digits, 
- *        in ascending order, separated by a comma followed by a space
+ * main - prints all possible combinations of two different digits, in ascending order, separated by a comma followed by a space
  *
  * Return: Always 0.
  */
@@ -10,7 +9,8 @@ int main(void)
 int digit1, digit2;
 
 for (digit1 = 0; digit1 < 9; digit1++)
-{for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+{
+for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 {
 putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
