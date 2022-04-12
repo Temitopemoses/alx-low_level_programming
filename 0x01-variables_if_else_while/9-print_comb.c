5341b6cdf2486241e5dd8d5b7a79a879ba70c4af
Temitopemoses <mosesogunbameru@gmail.com>
2022-04-12 08:49:08 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description: Prints all single digit number of base 10
+ * Description: Prints all single digit numbers of base 10
  * starting from 0, followed by a new line
  * Return: 0
  */
@@ -17,7 +17,7 @@ if (c != 9)
 putchar(',');
 putchar(' ');
 }
-c++
+c++;
 }
 putchar('\n');
 return (0);
