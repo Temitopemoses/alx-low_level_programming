acafd8018cdaa9cd1ab3da4cdf83d014cb579485
Temitopemoses <mosesogunbameru@gmail.com>
2022-04-12 02:57:09 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,8 +12,8 @@ int main(void)
 {
 int n;
 srand(time(0));
-n = rand() - RAND_MAx / 2;
-printf("last digit of %d is %d %s\n", n, n % 10,
+n = rand() - RAND_MAX / 2;
+printf("Last digit of %d is %d %s\n", n, n % 10,
 ((n % 10) == 0) ? "and is 0"
 : (((n % 10) > 5) ? "and is greater than 5"
 : "and is less than 6 and not 0"));
