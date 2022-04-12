32410be26b9ccb20eb6cd2a466dd9c35a1243775
Temitopemoses <mosesogunbameru@gmail.com>
2022-04-12 05:32:55 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: Print lowercase alphabet backwards
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; ++c)
+putchar(c);
+putchar('\n');
+
+return (0);
+}
