be1f48cadafe3eecaa7927611710d24ce6de6c30
Temitopemoses <mosesogunbameru@gmail.com>
2022-04-12 05:11:13 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,7 +2,7 @@
 /**
  * main - entry point
  *
- * Description: print numbers 0 to 9 on the line
+ * Description: Print numbers 0 to 9 on the line
  * Return: 0
  */
 int main(void)
@@ -10,7 +10,7 @@ int main(void)
 int i;
 for (i = 0; i < 10; ++i)
 putchar('0' + i);
-putchar('n\');
+putchar('\n');
 
 return (0);
 }
