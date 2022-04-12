32737627cb5085a1c51a62a7ffb18bc14054dbcb
Temitopemoses <mosesogunbameru@gmail.com>
2022-04-12 05:00:46 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print numbers 0 to 9 on the line
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; ++i)
+putchar('0' + i);
+putchar('n\');
+
+return (0);
+}
