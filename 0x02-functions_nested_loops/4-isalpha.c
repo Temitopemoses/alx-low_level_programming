eab173b0e238a4a3e2b30b6c71451a7d9d0cd914
Temitopemoses <101447242+Temitopemoses@users.noreply.github.com>
2022-04-13 09:58:33 -0700

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,3 +1,15 @@
 #include "main.h"
 /**
+ * _isalpha- checks if a character is lowercase.
+ * @c: The character to be checked
  *
+ * Return: 1 if character is lowercase, 0 otherwise.
+ */
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') ||
+(c >= 'A' && c<= 'Z'))
+return (1);
+else
+return (0);
+}
