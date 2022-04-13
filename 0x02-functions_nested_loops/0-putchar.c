17eb51c2c33371ae442f67186fcf059e61dfc13f
Temitopemoses <mosesogunbameru@gmail.com>
2022-04-13 09:05:48 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - main block
+ * Description: print -putchar in a new line
+ * Return: this program return zero
+ */
+int main(void)
+{
+_putchar('_');
+_putchara('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifindef _MAIN_H_
-#define _MAIN_H_
+#ifndef _MAIN_H
+#define _MAIN_H
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -10,8 +10,8 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void times_table(void);
+void times_table(void)
 int add(int, int);
-voiod print_to_998(int n);
+void print_to_98(int n);
 void print_times_table(int n);
-#endif /* MAIN_H*/
+#endif /* _MAIN_H_ */
