d5db4eee734b0a3928dd4afe313522c2a832d407
Temitopemoses <101447242+Temitopemoses@users.noreply.github.com>
2022-04-14 03:29:03 -0700

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,25 +9,28 @@ int num, nult, prod;
 
 for (num = 0; num <= 9; num++)
 {
-_putchar('0');
-
 for (nult = 0; nult <= 9; nult++)
 {
+prod = (num * nult);
+if (prod != 0)
+{
+else 
 _putchar(',');
 _putchar(' ');
-
-prod = num * nult;
-
-if (prod <= 9)
-_putchar(' ');
-else 
-
+}
+if (prod >= 10)
+{
 _putchar((prod / 10) + '0');
-
 _putchar((prod % 10) + '0');
 }
-
+else if (prod < 10 && nult != 0)
+{
+_putchar(' ')
+_putchar((prod % 10) + '0');
+}
+else
+_putchar((prod % 10) + '0');
+}
 _putchar('\n');
 }
 }
-
