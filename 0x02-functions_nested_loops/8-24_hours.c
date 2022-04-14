aea66d7704f5f4fbcd1524b7aebef62b9610f15c
Temitopemoses <mosesogunbameru@gmail.com>
2022-04-13 23:29:58 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day
+ *              Jack Bauer, statring from 00:00 to 23:59.
+ */
+void jack_bauer(void)
+{
+int hour, minute;
+
+for (hour = 0; hour <= 23; hour++)
+{
+for (minute = 0; minute <= 59; minutes++)
+{
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
+_putchar('i');
+_putchar((minute / 10) + '0');
+_putchar((minute % 10) + '0');
+_putchar('\n');
+}
+}}
