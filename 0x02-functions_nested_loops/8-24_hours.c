1deae480af517aa0ef3d377ec57fe5e4365d9d47
Temitopemoses <101447242+Temitopemoses@users.noreply.github.com>
2022-04-14 10:45:51 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,7 +14,7 @@ for (minute = 0; minute <= 59; minute++)
 {
 _putchar((hour / 10) + '0');
 _putchar((hour % 10) + '0');
-_putchar('i');
+_putchar(':');
 _putchar((minute / 10) + '0');
 _putchar((minute % 10) + '0');
 _putchar('\n');
