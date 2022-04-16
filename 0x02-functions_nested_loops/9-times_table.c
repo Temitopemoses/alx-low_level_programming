bb2d4bae466603d380cf0f61f295d01d9d0ff1b8
Temitopemoses <101447242+Temitopemoses@users.noreply.github.com>
2022-04-16 02:39:41 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,35 +1,30 @@
-#include "main.h"
-
-/**
- * times_table - Prints the 9 times table starting with 0
- */
-void times_table(void)
-{
-int num, nult, prod;
-
-for (num = 0; num <= 9; num++)
-{
-for (nult = 0; nult <= 9; nult++)
-{
-prod = (num * nult);
-if (prod != 0)
-{
-_putchar(',');
-_putchar(' ');
-}
-if (prod >= 10)
-{
-_putchar((prod / 10) + '0');
-_putchar((prod % 10) + '0');
-}
-else if (prod < 10 && nult != 0)
-{
-_putchar(' ');
-_putchar((prod % 10) + '0');
-}
-else
-_putchar((prod % 10) + '0');
-}
-_putchar('\n');
-}
-}
+#include "main.h"                                                                                                               
+                                                                                                                                
+/**                                                                                                                             
+ * times_table - Print the 9 times table starting with 0                                                                        
+ */                                                                                                                             
+void times_table(void)                                                                                                          
+{                                                                                                                               
+        int num, mult, prod;                                                                                                    
+                                                                                                                                
+        for (num = 0; num <= 9; num++)                                                                                          
+        {                                                                                                                       
+        _putchar('0');                                                                                                          
+                                                                                                                                
+        for (mult = 1; mult <= 9; mult++)                                                                                       
+        {                                                                                                                       
+        _putchar(',');                                                                                                          
+        _putchar(' ');                                                                                                          
+                                                                                                                                
+        prod = mult * num;                                                                                                      
+                                                                                                                                
+        if (prod <= 9)                                                                                                          
+        _putchar(' ');                                                                                                          
+        else                                                                                                                    
+        _putchar((prod / 10) + '0');                                                                                            
+                                                                                                                                
+        _putchar((prod % 10) + '0');                                                                                            
+        }                                                                                                                       
+        _putchar('\n');                                                                                                         
+        }                                                                                                                       
+} 
