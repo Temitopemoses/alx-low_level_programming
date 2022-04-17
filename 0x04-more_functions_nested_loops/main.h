69e27e8590710247e3939e548ff07879d4bf237c
Temitopemoses <mosesogunbameru@gmail.com>
2022-04-17 05:35:38 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_number(int n);
+void print_numbers(void);
+#endif /*_MAIN_H_ */
