1525cb0d86c1d7bb91d0e8cb2fdaf00fa7f21777
Temitopemoses <mosesogunbameru@gmail.com>
2022-04-17 06:45:23 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the number, from  0-14
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+int i, j, a;
+
+for (a = 0; a < 10; a++)
+{
+for (i =
