e6a4e7116e16163d5fe17a257fcbee1fc5608116
Temitopemoses <101447242+Temitopemoses@users.noreply.github.com>
2022-04-18 08:52:04 -0700

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,4 +12,18 @@ int i, j, a;
 
 for (a = 0; a < 10; a++)
 {
-for (i =
+for (i = 0; i <= 1, i++)
+{
+for (j = 0; j <= 9, j++)
+{
+if (!(i == 1 && j >= 5))
+{
+if (i)
+_putchar(i + '0');
+_putchar(j + '0');
+}
+}
+}
+_putchar('\n')
+}
+}
