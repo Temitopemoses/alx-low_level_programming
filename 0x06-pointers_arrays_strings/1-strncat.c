91690236c27b54582a826565ede2c5e5645f709b
Temitopemoses <mosesogunbameru@gmail.com>
2022-04-22 22:19:01 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strncpy - function that copies a string
+ * @dest: string to copy to
+ * @src: string to be copied from
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int x;
+
+for (x = 0; x < n && src[x] != '\0'; x++)
+dest[x] = src[x];
+for (; n > x; x++)
+dest[x] = '\0';
+
+return (dest);
+}
