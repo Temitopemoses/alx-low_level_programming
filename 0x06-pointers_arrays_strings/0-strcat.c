463f7c40c6825e34b93fce5d2ec59099cd163b91
Temitopemoses <101447242+Temitopemoses@users.noreply.github.com>
2022-04-24 09:01:06 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,30 @@
 #include "main.h"
+
 /**
  * *_strcat - concatenates two strings
  * @dest: second string to be concatenated to src
  * @src: first string to be concatenated to dest
  * Return: pointer to the resulting string dest
  */
+
 char *_strcat(char *dest, char *src)
 {
-int x, y;
+int i, j;
 
-for (x = 0; dest[x] != '\0'; x++);
-for (y = 0; src[y] != '\0'; y++)
+i = 0;
+while (ddest[i] != '\0')
 {
-dest[x] = src[y];
-x++;
+i++;
 }
-dest[x] = '\0';
+
+j = 0;
+while (src[j] != '\0')
+{
+dest[i] = src [j];
+j++;
+i++
+}
+dest[i] = '\0';
+
 return (dest);
 }
