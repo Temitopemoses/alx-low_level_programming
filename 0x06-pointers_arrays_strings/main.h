4ebc5cd04b7abcc017676b5ec1b659504664f57f
Temitopemoses <101447242+Temitopemoses@users.noreply.github.com>
2022-04-24 09:53:12 -0700

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,6 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
-int _putchar(char c);
 char *_Strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -9,4 +8,5 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
+int _putchar(char c);
 #endif /* _MAIN_H */
